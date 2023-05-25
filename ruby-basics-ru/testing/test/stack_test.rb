993fac0a8b9b41380309f3c6d4a80d674c9560cd
@hexlet/cli <support@hexlet.io>
2023-05-25 14:13:21 +0300

    submit ruby-basics-ru/testing

---
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative '../lib/stack'
+
+class StackTest < Minitest::Test
+  # BEGIN
+  def test_power_asssert
+    assert { true }
+  end
+  # END
+end
+
+test_methods = StackTest.new({}).methods.select { |method| method.start_with? 'test_' }
+raise 'StackTest has not tests!' if test_methods.empty?
